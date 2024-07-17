5a3831c3bac290bb2b8693ba4e25c883e6d7b9f2
Xinyi Liu <94362768+XinyiLiu577086410@users.noreply.github.com>
2024-07-17 10:23:27 +0800

    Update winograd.c
    Fix bugs in filter tranform, validation passed when transpose the tensor back and test in small.conf.

--- winograd.c
+++ winograd.c
@@ -94,7 +94,7 @@ void sgemm_4x6x6(const float *A, const float *B, float *out, const int M, const 
 
 void sgemm_4x6x4(const float *A, const float *B, float *out, const int M, const int K, const int N)
 {
-  for (int i = 0; i < 16; ++i){
+  for (int i = 0; i < 16; ++i) {
     out[i] = 0.0f;
   }
   for (int i = 0; i < 4; ++i)
@@ -103,22 +103,30 @@ void sgemm_4x6x4(const float *A, const float *B, float *out, const int M, const 
         out[i * 4 + j] += A[i * 6 + k] * B[k * 4 + j];
 }
 
-void filter_KCHW_to_KHWC(float* __restrict__ filter, int K, 
-                                    int C, int filer_H, int filer_W, 
-                                    float* __restrict__ filer_KHWC) {
-  PRAGMA_OMP_PARALLEL_FOR()
+void filter_KCHW_to_KHWC(float* __restrict__ filter, int K, int C, float* __restrict__ filer_KHWC) {
+  PRAGMA_OMP_PARALLEL_FOR_COLLAPSE(2)
   for(int k = 0; k < K; ++k)
-    for(int c = 0; c < C; ++c)
-      for(int h = 0; h < filer_H; ++h)
-        for(int w = 0; w < filer_W; ++w) {
-          // 多线程集中读分散写，避免伪共享
-          filer_KHWC[k * filer_H * filer_W * C + h * filer_W * C + w * C + c] 
-                    = filter[k * C * filer_H * filer_W + c * filer_H * filer_W + h * filer_W + w];
+    for(int h = 0; h < FLT_HW; ++h)
+      for(int w = 0; w < FLT_HW; ++w) {
+        for(int c = 0; c < C; ++c)
+          // 注意伪共享
+          filer_KHWC[k * 3 * 3 * C + h * 3 * C + w * C + c] 
+                    = filter[k * C * 3 * 3 + c * 3 * 3 + h * 3 + w];
         }
 }
 
+void u_arr_K66C_to_KC66(float* __restrict__ u_arr, int K, int C, float* __restrict__ u_arr_out) {
+  PRAGMA_OMP_PARALLEL_FOR()
+  for(int k = 0; k < K; ++k)
+    for(int h = 0; h < TILE_IN_HW; ++h)
+      for(int w = 0; w < TILE_IN_HW; ++w) {
+        for(int c = 0; c < C; ++c)
+          u_arr_out[k * C * 6 * 6 + c * 6 * 6 + h * 6 + w] 
+                    = u_arr[k * 6 * 6 * C + h * 6 * C + w * C + c];
+        }
+}
 
-void filter_transform(float* __restrict__ filer_KHWC, int K, int filer_H, int filer_W, int C, float* __restrict__ u_arr) {
+void filter_transform(float* __restrict__ filer_KHWC, int K,  int C, float* __restrict__ u_arr) {
   // filer_KHWC[K][3][3][C]
   // u_arr[K][6][6][C]
   float* tmp_u = malloc(sizeof(float) * K * 6 * 3 * C); 
@@ -126,7 +134,7 @@ void filter_transform(float* __restrict__ filer_KHWC, int K, int filer_H, int fi
   for (int k = 0; k < K; ++k) {
     float* filter_ptr = filer_KHWC + k * 3 * 3 * C;
     float* u_arr_ptr = u_arr + k * 6 * 6 * C;
-    float* tmp_u_ptr = k * 6 * 3 * C;
+    float* tmp_u_ptr = tmp_u + k * 6 * 3 * C;
     DECLARE_SVE_FP32_REGS();
     z25 = svdup_f32(1.0f);
     z26 = svdup_f32(- 1.0f / 6.0f );
@@ -138,7 +146,7 @@ void filter_transform(float* __restrict__ filer_KHWC, int K, int filer_H, int fi
     for(int c = 0; c < C; c += FP32_PER_REG) {
       svbool_t pg = svwhilelt_b32(0, MIN(FP32_PER_REG, C-c));
       // G * filter
-      for (int i = 0; i < FLT_W; ++i) {     // 遍历列
+      for (int i = 0; i < FLT_HW; ++i) {     // 遍历列
         z6 = svld1(pg, filter_ptr + i * 3 * C + 0 * C + c);
         
         z0 = svmul_f32_x(pg, z28, z6);
@@ -172,7 +180,7 @@ void filter_transform(float* __restrict__ filer_KHWC, int K, int filer_H, int fi
         svst1_f32(pg, tmp_u_ptr +  5 * 3 * C + i * C + c, z5);
       }
       // filter * G_T
-      for (int i = 0; i < TILE_OUT_W; ++i) {
+      for (int i = 0; i < TILE_IN_HW; ++i) {
         z6 = svld1(pg, tmp_u_ptr + i * 3 * C + 0 * C + c);
         
         z0 = svmul_f32_x(pg, z28, z6);
@@ -210,8 +218,6 @@ void filter_transform(float* __restrict__ filer_KHWC, int K, int filer_H, int fi
   free(tmp_u);
 }
 
-
-
 // User API for winograd F(2,3)
 // image: [batch * C * inHeight * inWidth]
 // filter: [K * C * 3 * 3]
@@ -236,18 +242,22 @@ void winconv_2x3(float *__restrict__ image, const int inHeight,
   memset(out, 0, N * outgap[0] * sizeof(float));
 
   float* u_arr = (float*) malloc(sizeof(float) * K * 6 * 6 * C);
+  float* u_arr_trans = (float*) malloc(sizeof(float) * K * 6 * 6 * C);
   assert(u_arr != NULL);
-
+  assert(u_arr_trans != NULL);
   float* filer_KHWC = (float*) malloc(K * C * 3 * 3 * sizeof(float));
   assert(filer_KHWC != NULL);
-  filter_KCHW_to_KHWC(filter, K, C, 3, 3, filer_KHWC);
 
-  filter_transform(filer_KHWC, K, 3, 3, C, u_arr);
+  filter_KCHW_to_KHWC(filter, K, C, filer_KHWC);
 
+  filter_transform(filer_KHWC, K, C, u_arr);
 
-  // U[:, :, k, c] = G * filters[k, c, :, :] * G.T()
-  #pragma omp parallel for collapse(3)// private(tmp_u, u)
-  // #pragma omp parallel for collapse(2)// private(tmp_v, d, v)
+  u_arr_K66C_to_KC66(u_arr, K, C, u_arr_trans);
+
+  free(u_arr);
+  u_arr = u_arr_trans;
+
+  #pragma omp parallel for collapse(3)
   for (int n = 0; n < N; ++n) {
     for (int y = 0; y < outHeight / 4; ++y) {
       for (int x = 0; x < outWidth / 4; ++x) {
