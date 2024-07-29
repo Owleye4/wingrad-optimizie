bb34f8ed05efe12210bd5477780328932d349deb
lxy <577086410@qq.com>
2024-07-29 22:26:31 +0800

    Fix: useless export OMP_NUM_THREADS in run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,4 @@
 #SBATCH -c 160
 #SBATCH --exclusive
 export OMP_NUM_THREADS=160
-export LD_LIBRARY_PATH="/shareofs/apps/libs/kml/2.2.0-bisheng3.2.0/lib/kblas/omp:${LD_LIBRARY_PATH}"
 numactl --cpunodebind=0-3 --membind=0-3 ./winograd conf/final.conf
