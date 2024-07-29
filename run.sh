3c12538a520a242b65086885e7ffc5e91a78e17c
lxy <577086410@qq.com>
2024-07-29 12:55:23 +0800

    All address calc now uses int64_t, validation on final passed

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@
 #SBATCH -c 160
 #SBATCH --exclusive
 export OMP_NUM_THREADS=160
-numactl --cpunodebind=0-3 --membind=0-3 perf stat -ddd ./winograd resnet101.conf 1
+export LD_LIBRARY_PATH="/shareofs/apps/libs/kml/2.2.0-bisheng3.2.0/lib/kblas/omp:${LD_LIBRARY_PATH}"
+numactl --cpunodebind=0-3 --membind=0-3 ./winograd conf/final.conf
--- winograd.c
+++ winograd.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,65 +7,32 @@
 #include "common.h"
 #include "kblas.h"
 ALWAYS_INLINE void filterIcPack(float* __restrict__ filter, FltShape fs, float* __restrict__ packedFiler) {
-  int  K = fs.oc, C = fs.ic;
-  PRAGMA_OMP_PARALLEL_FOR_COLLAPSE(3)
-  for(int  k = 0; k < K; ++k)
-    for(int  h = 0; h < FLT_HW; ++h)
-      for(int  w = 0; w < FLT_HW; ++w)
-        for(int  c = 0; c < C; ++c)
-          packedFiler[k * 3 * 3 * C + h * 3 * C + w * C + c] 
-                    = filter[k * C * 3 * 3 + c * 3 * 3 + h * 3 + w];
-}
-
-ALWAYS_INLINE void ImageIcPack(float* __restrict__ Image, ImgShape is,  float* __restrict__ packedImage) {
-  int  N = is.numImg, C = is.ic, H = is.h, W = is.w;
-  PRAGMA_OMP_PARALLEL_FOR_COLLAPSE(2)
-  for(int  n = 0; n < N; ++n)
-    for(int  h = 0; h < H; ++h)
-      for(int  c = 0; c < C; ++c)
-        for(int  w = 0; w < W; ++w)
-          packedImage[n * H * W * C + h * W * C + w * C + c] 
-                    = Image[n * C * H * W + c * H * W + h * W + w];
-}
-
-ALWAYS_INLINE void filterOcIcPack(float* __restrict__ filter, FltShape fs, float* __restrict__ packedFiler) {
-  int  outChannel = fs.oc, inChannel = fs.ic;
-  typedef float (*packedFilerTensor_t) [FLT_W][outChannel][inChannel];
-  typedef float (*filterTensor_t) [inChannel][FLT_H][FLT_W];
+  typedef float (*packedFilerTensor_t) [FLT_H][FLT_W][fs.ic];
+  typedef float (*filterTensor_t) [fs.ic][FLT_H][FLT_W];
   packedFilerTensor_t packedFilerTensor = (packedFilerTensor_t) packedFiler;
   filterTensor_t filterTensor = (filterTensor_t) filter;
-  PRAGMA_OMP_PARALLEL_FOR_COLLAPSE(2)
-  for(int  k = 0; k < outChannel; ++k)
+  PRAGMA_OMP_PARALLEL_FOR_COLLAPSE(3)
+  for(int  k = 0; k < fs.oc; ++k)
     for(int  h = 0; h < FLT_HW; ++h)
       for(int  w = 0; w < FLT_HW; ++w)
-        for(int  c = 0; c < inChannel; ++c) {
-          packedFilerTensor[h][w][k][c] = filterTensor[k][c][h][w];
-        }
+        for(int  c = 0; c < fs.ic; ++c)
+          packedFilerTensor[k][h][w][c] = filterTensor[k][c][h][w];
 }
 
-ALWAYS_INLINE void ImageTileIcPack(float* __restrict__ image, ImgShape is,  float* __restrict__ packedImage,  TileShape ts) {
-  int  batchSize = is.numImg, inputChannels = is.ic, imgHeight = is.h, imgWidth = is.w, numTileTotal = ts.numTileTotal;
-  typedef float (*ImgTensor_t) [inputChannels][imgHeight][imgWidth];
-  typedef float (*packedImgTensor_t) [TILE_IN_W][numTileTotal][inputChannels];
+ALWAYS_INLINE void ImageIcPack(float* __restrict__ image, ImgShape is,  float* __restrict__ packedImage) {
+  typedef float (*packedImageTensor_t) [is.h][is.w][is.ic];
+  typedef float (*imageTensor_t) [is.ic][is.h][is.w];
+  packedImageTensor_t packedImageTensor = (packedImageTensor_t) packedImage;
+  imageTensor_t imageTensor = (imageTensor_t) image;
   PRAGMA_OMP_PARALLEL_FOR_COLLAPSE(2)
-  for(int  tile = 0; tile < numTileTotal; ++tile)
-    for(int  ic = 0; ic < inputChannels; ++ic)
-      for(int  h = 0; h < TILE_IN_H; ++h)
-        for(int  w = 0; w < TILE_IN_W; ++w) {
-          TileIndex ti = getTileIndex(tile, ts);
-          int  b = ti.b, x = ti.tw, y = ti.th;
-          packedImgTensor_t packedImgTensor = (packedImgTensor_t) packedImage;
-          ImgTensor_t imgTensor = (ImgTensor_t) image;
-          if(y * 4 + h < imgHeight && x * 4 + w < imgWidth)
-            packedImgTensor[h][w][tile][ic] = imgTensor[b][ic][y * 4 + h][x * 4 + w];
-          else
-            packedImgTensor[h][w][tile][ic] = 0;
-        }
+  for(int  n = 0; n < is.numImg; ++n)
+    for(int  h = 0; h < is.h; ++h)
+      for(int  c = 0; c < is.ic; ++c)
+        for(int  w = 0; w < is.w; ++w)
+          packedImageTensor[n][h][w][c] = imageTensor[n][c][h][w];
 }
 
 ALWAYS_INLINE void filterTransformSVE(float* __restrict__ packedFilter, float* __restrict__ U, UShape us, int  simdDimSize, int  simdDimIndex) {
-  // float* filter_ptr = packedFilter + k * 3 * 3 * C;
-  // float* u_arr_ptr = U + k * 6 * 6 * C;
   typedef float (*packedFilterTensor_t) [FLT_W][simdDimSize];
   typedef float (*UTensor_t) [TILE_IN_W][simdDimSize];
   packedFilterTensor_t packedFilterTensor = (packedFilterTensor_t) packedFilter;
@@ -149,128 +117,15 @@ ALWAYS_INLINE void filterTransformSVE(float* __restrict__ packedFilter, float* _
   }
 }
 
-ALWAYS_INLINE void tileIcPackedSrcTransformSVE(float* __restrict__ packedImage, float* __restrict__ V, VShape vs, int  simdDimSize, int  simdDimIndex) {
-  int  inputChannels = vs.ic, numTileTotal = vs.numTileTotal;
-  typedef float(*VTensor_t)[TILE_IN_W][simdDimSize];
-  typedef float(*packedImageTensor_t)[TILE_IN_W][simdDimSize];
-  VTensor_t VTensor = (VTensor_t) V;
-  packedImageTensor_t packedImageTensor = (packedImageTensor_t) packedImage;
-  svbool_t pg = svwhilelt_b32(0, MIN(FP32_PER_REG, simdDimSize-simdDimIndex));
-  float tmp[TILE_IN_H][TILE_IN_W][FP32_PER_REG] ATTRIBUTE_ALIGN(128);
-  DECLARE_SVE_FP32_REGS();
-  z22 = svdup_f32( -8.0f );
-  z23 = svdup_f32(  8.0f );
-  z24 = svdup_f32(  1.0f );
-  z25 = svdup_f32( -1.0f );
-  z26 = svdup_f32(  2.0f );
-  z27 = svdup_f32( -2.0f );
-  z28 = svdup_f32(  4.0f );
-  z29 = svdup_f32( -4.0f );
-  z30 = svdup_f32(  5.0f );
-  z31 = svdup_f32( -5.0f );
-  for (int  w = 0; w < TILE_IN_W; ++w) {   // 按列产生结果。
-    z6 = svld1(pg, &packedImageTensor[0][w][simdDimIndex]);
-
-    z0 = svmul_f32_x(pg, z28, z6);
-
-    z6 = svld1(pg, &packedImageTensor[1][w][simdDimIndex]);
-
-    z1 = svmul_f32_x(pg, z29, z6);
-    z2 = svmul_f32_x(pg, z28, z6);
-    z3 = svmul_f32_x(pg, z27, z6);
-    z4 = svmul_f32_x(pg, z26, z6);
-    z5 = svmul_f32_x(pg, z28, z6);
-
-    z6 = svld1(pg, &packedImageTensor[2][w][simdDimIndex]);
-
-    z0 = svmla_f32_x(pg, z0, z31, z6);
-    z1 = svmla_f32_x(pg, z1, z29, z6);
-    z2 = svmla_f32_x(pg, z2, z29, z6);
-    z3 = svmla_f32_x(pg, z3, z25, z6);
-    z4 = svmla_f32_x(pg, z4, z25, z6);
-
-    z6 = svld1(pg, &packedImageTensor[3][w][simdDimIndex]);
-
-    z1 = svmla_f32_x(pg, z1, z24, z6);
-    z2 = svmla_f32_x(pg, z2, z25, z6);
-    z3 = svmla_f32_x(pg, z3, z26, z6);
-    z4 = svmla_f32_x(pg, z4, z27, z6);
-    z5 = svmla_f32_x(pg, z5, z31, z6);
-
-    z6 = svld1(pg, &packedImageTensor[4][w][simdDimIndex]);
-
-    z0 = svmla_f32_x(pg, z0, z24, z6);
-    z1 = svmla_f32_x(pg, z1, z24, z6);
-    z2 = svmla_f32_x(pg, z2, z24, z6);
-    z3 = svmla_f32_x(pg, z3, z24, z6);
-    z4 = svmla_f32_x(pg, z4, z24, z6);
-
-    z6 = svld1(pg, &packedImageTensor[5][w][simdDimIndex]);
-
-    z5 = svmla_f32_x(pg, z5, z24, z6);
-
-    svst1_f32(pg, tmp[0][w], z0);
-    svst1_f32(pg, tmp[1][w], z1);
-    svst1_f32(pg, tmp[2][w], z2);
-    svst1_f32(pg, tmp[3][w], z3);
-    svst1_f32(pg, tmp[4][w], z4);
-    svst1_f32(pg, tmp[5][w], z5);
-  }
-
-  for (int  h = 0; h < TILE_IN_H; ++h) {   // 按行产生结果。
-    z6 = svld1(pg, tmp[h][0]);
-
-    z0 = svmul_f32_x(pg, z28, z6);
-
-    z6 = svld1(pg, tmp[h][1]);
-
-    z1 = svmul_f32_x(pg, z29, z6);
-    z2 = svmul_f32_x(pg, z28, z6);
-    z3 = svmul_f32_x(pg, z27, z6);
-    z4 = svmul_f32_x(pg, z26, z6);
-    z5 = svmul_f32_x(pg, z28, z6);
-
-    z6 = svld1(pg, tmp[h][2]);
-
-    z0 = svmla_f32_x(pg, z0, z31, z6);
-    z1 = svmla_f32_x(pg, z1, z29, z6);
-    z2 = svmla_f32_x(pg, z2, z29, z6);
-    z3 = svmla_f32_x(pg, z3, z25, z6);
-    z4 = svmla_f32_x(pg, z4, z25, z6);
-
-    z6 = svld1(pg, tmp[h][3]);
-
-    z1 = svmla_f32_x(pg, z1, z24, z6);
-    z2 = svmla_f32_x(pg, z2, z25, z6);
-    z3 = svmla_f32_x(pg, z3, z26, z6);
-    z4 = svmla_f32_x(pg, z4, z27, z6);
-    z5 = svmla_f32_x(pg, z5, z31, z6);
-
-    z6 = svld1(pg, tmp[h][4]);
-    
-    z0 = svmla_f32_x(pg, z0, z24, z6);
-    z1 = svmla_f32_x(pg, z1, z24, z6);
-    z2 = svmla_f32_x(pg, z2, z24, z6);
-    z3 = svmla_f32_x(pg, z3, z24, z6);
-    z4 = svmla_f32_x(pg, z4, z24, z6);
-
-    z6 = svld1(pg, tmp[h][5]);
-
-    z5 = svmla_f32_x(pg, z5, z24, z6);
-
-    svst1_f32(pg, &VTensor[h][0][simdDimIndex], z0);
-    svst1_f32(pg, &VTensor[h][1][simdDimIndex], z1);
-    svst1_f32(pg, &VTensor[h][2][simdDimIndex], z2);
-    svst1_f32(pg, &VTensor[h][3][simdDimIndex], z3);
-    svst1_f32(pg, &VTensor[h][4][simdDimIndex], z4);
-    svst1_f32(pg, &VTensor[h][5][simdDimIndex], z5);
-  }
-}
 
 ALWAYS_INLINE void srcPaddingAndTransformSVE(float* __restrict__ packedImage, ImgShape is,  float* V, VShape vs, int  tileNo, TileShape ts, int  c) {
   TileIndex ti = getTileIndex(tileNo, ts);
-  int  n = ti.b, x = ti.tw, y = ti.th;
-  int  inHeight = is.h, inWidth = is.w, C = is.ic;
+  int64_t  n = ti.b, x = ti.tw, y = ti.th;
+  int64_t  inHeight = is.h, inWidth = is.w, C = is.ic;
+  typedef float (*packedImageTensor_t) [is.h][is.w][is.ic];
+  typedef float (*VSlicedTensor_t) [TILE_IN_W][is.ic];
+  packedImageTensor_t packedImageTensor = (packedImageTensor_t) packedImage;
+  VSlicedTensor_t VTensor = (VSlicedTensor_t) (V + tileNo * TILE_IN_H * TILE_IN_W * is.ic);
   DECLARE_SVE_FP32_REGS();
   z22 = svdup_f32( -8.0f );
   z23 = svdup_f32(  8.0f );
@@ -284,18 +139,16 @@ ALWAYS_INLINE void srcPaddingAndTransformSVE(float* __restrict__ packedImage, Im
   z31 = svdup_f32( -5.0f );
   float tmp[TILE_IN_H][TILE_IN_W][FP32_PER_REG] ATTRIBUTE_ALIGN(128);
   memset((void*) tmp, 0, sizeof(tmp));
-  svbool_t pg = svwhilelt_b32(0, MIN(FP32_PER_REG, C-c));
+  svbool_t pg = svwhilelt_b32(0L, MIN(FP32_PER_REG, is.ic - c));
 
-  float (*Varr) [TILE_IN_W][C] = (float (*)[TILE_IN_W][C]) (V + tileNo * TILE_IN_H * TILE_IN_W * C);
-
-  for (int  xx = 0; xx < TILE_IN_W && (x * 4 + xx) < inWidth; ++xx) {   // 按列产生结果。
+  for (int  xx = 0; xx < TILE_IN_W && (x * 4 + xx) < is.w; ++xx) {   // 按列产生结果。
     if((y * 4 + 0) < inHeight){
-      z6 = svld1(pg, packedImage + n * inHeight * inWidth * C + (y * 4 + 0) * inWidth * C + (x * 4 + xx) * C + c);
+      z6 = svld1(pg, &packedImageTensor[n][y * 4 + 0][x * 4 + xx][c]);
       z0 = svmul_f32_x(pg, z28, z6);
     }
 
-    if((y * 4 + 1) < inHeight) {
-      z6 = svld1(pg, packedImage + n * inHeight * inWidth * C + (y * 4 + 1) * inWidth * C + (x * 4 + xx) * C + c);
+    if((y * 4 + 1) < is.h) {
+      z6 = svld1(pg, &packedImageTensor[n][y * 4 + 1][x * 4 + xx][c]);
       z1 = svmul_f32_x(pg, z29, z6);
       z2 = svmul_f32_x(pg, z28, z6);
       z3 = svmul_f32_x(pg, z27, z6);
@@ -303,8 +156,8 @@ ALWAYS_INLINE void srcPaddingAndTransformSVE(float* __restrict__ packedImage, Im
       z5 = svmul_f32_x(pg, z28, z6);
     }
 
-    if((y * 4 + 2) < inHeight) {
-      z6 = svld1(pg, packedImage + n * inHeight * inWidth * C + (y * 4 + 2) * inWidth * C + (x * 4 + xx) * C + c);
+    if((y * 4 + 2) < is.h) {
+      z6 = svld1(pg, &packedImageTensor[n][y * 4 + 2][x * 4 + xx][c]);
       z0 = svmla_f32_x(pg, z0, z31, z6);
       z1 = svmla_f32_x(pg, z1, z29, z6);
       z2 = svmla_f32_x(pg, z2, z29, z6);
@@ -312,8 +165,8 @@ ALWAYS_INLINE void srcPaddingAndTransformSVE(float* __restrict__ packedImage, Im
       z4 = svmla_f32_x(pg, z4, z25, z6);
     }
 
-    if((y * 4 + 3) < inHeight) {
-      z6 = svld1(pg, packedImage + n * inHeight * inWidth * C + (y * 4 + 3) * inWidth * C + (x * 4 + xx) * C + c);
+    if((y * 4 + 3) < is.h) {
+      z6 = svld1(pg, &packedImageTensor[n][y * 4 + 3][x * 4 + xx][c]);
       z1 = svmla_f32_x(pg, z1, z24, z6);
       z2 = svmla_f32_x(pg, z2, z25, z6);
       z3 = svmla_f32_x(pg, z3, z26, z6);
@@ -321,8 +174,8 @@ ALWAYS_INLINE void srcPaddingAndTransformSVE(float* __restrict__ packedImage, Im
       z5 = svmla_f32_x(pg, z5, z31, z6);
     }
 
-    if((y * 4 + 4) < inHeight) {
-      z6 = svld1(pg, packedImage + n * inHeight * inWidth * C + (y * 4 + 4) * inWidth * C + (x * 4 + xx) * C + c);
+    if((y * 4 + 4) < is.h) {
+      z6 = svld1(pg, &packedImageTensor[n][y * 4 + 4][x * 4 + xx][c]);
       z0 = svmla_f32_x(pg, z0, z24, z6);
       z1 = svmla_f32_x(pg, z1, z24, z6);
       z2 = svmla_f32_x(pg, z2, z24, z6);
@@ -330,8 +183,8 @@ ALWAYS_INLINE void srcPaddingAndTransformSVE(float* __restrict__ packedImage, Im
       z4 = svmla_f32_x(pg, z4, z24, z6);
     }
 
-    if((y * 4 + 5) < inHeight) {
-      z6 = svld1(pg, packedImage + n * inHeight * inWidth * C + (y * 4 + 5) * inWidth * C + (x * 4 + xx) * C + c);
+    if((y * 4 + 5) < is.h) {
+      z6 = svld1(pg, &packedImageTensor[n][y * 4 + 5][x * 4 + xx][c]);
       z5 = svmla_f32_x(pg, z5, z24, z6);
     }
 
@@ -384,12 +237,12 @@ ALWAYS_INLINE void srcPaddingAndTransformSVE(float* __restrict__ packedImage, Im
 
     z5 = svmla_f32_x(pg, z5, z24, z6);
 
-    svst1_f32(pg, &Varr[yy][0][c], z0);
-    svst1_f32(pg, &Varr[yy][1][c], z1);
-    svst1_f32(pg, &Varr[yy][2][c], z2);
-    svst1_f32(pg, &Varr[yy][3][c], z3);
-    svst1_f32(pg, &Varr[yy][4][c], z4);
-    svst1_f32(pg, &Varr[yy][5][c], z5);
+    svst1_f32(pg, &VTensor[yy][0][c], z0);
+    svst1_f32(pg, &VTensor[yy][1][c], z1);
+    svst1_f32(pg, &VTensor[yy][2][c], z2);
+    svst1_f32(pg, &VTensor[yy][3][c], z3);
+    svst1_f32(pg, &VTensor[yy][4][c], z4);
+    svst1_f32(pg, &VTensor[yy][5][c], z5);
   }
 }
 
@@ -403,7 +256,7 @@ ALWAYS_INLINE void filterTransform(float* __restrict__ packedFilter, float* __re
   }
 }
 
-ALWAYS_INLINE void destTransformSVE(float* __restrict__ M, float* __restrict__ Y, int  simdDimSize, int  simdDimIndex) {
+ALWAYS_INLINE void destTransformSVE(float* __restrict__ M, float* __restrict__ Y, int64_t  simdDimSize, int64_t  simdDimIndex) {
   
   typedef float (*MTensor_t)[TILE_IN_W][simdDimSize];
   typedef float (*YTensor_t)[TILE_IN_W][simdDimSize];
@@ -422,7 +275,7 @@ ALWAYS_INLINE void destTransformSVE(float* __restrict__ M, float* __restrict__ Y
   z29 = svdup_f32( -4.0f );
   z30 = svdup_f32(  5.0f );
   z31 = svdup_f32( -5.0f );
-  svbool_t pg = svwhilelt_b32(0, MIN(FP32_PER_REG, simdDimSize - simdDimIndex));
+  svbool_t pg = svwhilelt_b32(0L, MIN(FP32_PER_REG, simdDimSize - simdDimIndex));
   for (int  w = 0; w < TILE_IN_W; ++w) {   // 按列产生结果。
     z4 = svld1(pg, &MTensor[0][w][simdDimIndex]);
     
@@ -544,22 +397,17 @@ void winconv_2x3(float *__restrict__ image, const int  inHeight,
   TileShape ts = getTileShape(is, os);
   UShape us = getUShape(fs);
   VShape vs = getVShape(is, ts);
-  {
-    int  l = os.oc * os.h * os.w * numBatch;
-    #pragma omp parallel for simd aligned(out) schedule(static)
-    for(int  i = 0; i < l; ++i) out[i] = 0;
-  }
-  float* filerPacked =  (float*)  aligned_alloc(ALLOC_ALIGNMENT, numOutChannel * numInChannel * 3 * 3 * sizeof(float));
+  float* filerPacked =  (float*)  aligned_alloc(ALLOC_ALIGNMENT, numOutChannel * numInChannel * FLT_H * FLT_W * sizeof(float));
   assert(filerPacked != NULL);
   float* imagePacked =  (float*)  aligned_alloc(ALLOC_ALIGNMENT, sizeof(float) * numBatch * numInChannel * inHeight * inWidth);
   assert(imagePacked != NULL);
-  float* V = (float*)  aligned_alloc(ALLOC_ALIGNMENT, sizeof(float) * vs.numTileTotal  * 6 * 6 * numInChannel);
+  float* V = (float*)  aligned_alloc(ALLOC_ALIGNMENT, sizeof(float) * vs.numTileTotal  * TILE_IN_H * TILE_IN_W * numInChannel);
   assert(V != NULL);
-  float* U = (float*)  aligned_alloc(ALLOC_ALIGNMENT, sizeof(float) * numOutChannel * 6 * 6 * numInChannel);
+  float* U = (float*)  aligned_alloc(ALLOC_ALIGNMENT, sizeof(float) * numOutChannel * TILE_IN_H * TILE_IN_W * numInChannel);
   assert(U != NULL);
 
-  typedef float (*UTensor_t) [6][6][numInChannel];
-  typedef float (*VTensor_t) [6][6][numInChannel];
+  typedef float (*UTensor_t) [TILE_IN_H][TILE_IN_W][numInChannel];
+  typedef float (*VTensor_t) [TILE_IN_H][TILE_IN_W][numInChannel];
   UTensor_t UTensor = (UTensor_t) U;
   VTensor_t VTensor = (VTensor_t) V;
 
